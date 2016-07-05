489f3a725adfb66be3871e4ba7a16bd87b09ed86
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-07-05 11:57:53 +0700

    add rel callback

--- actions/callback.js
+++ actions/callback.js
@@ -1,8 +1,31 @@
+var fetchUrl = require("fetch").fetchUrl;
 
 var Callback = function (options) {
 
+    var baseUrl = 'http://call.mobilon.ru/';
+
+    var prepareOrderUrl = function (key, opts) {
+        var qs = '';
+        for (var prop in opts) {
+            if (opts.hasOwnProperty(prop)) {
+                qs += prop + '=' + opts[prop] + '&';
+            };
+        }; 
+
+        return [baseUrl, key, '/save?', qs].join("");
+    };
+
     var run = function (config, data) {
-        console.log('callback', config, data);
+        //console.log('callback', config, data);
+
+        var url = prepareOrderUrl(config.key, {
+            number: data[config.phoneField]
+        });
+        console.log('callback url:', url);
+        
+        fetchUrl(url, function (error, meta, body) {
+            console.log('callback result', body.toString());
+        });
     };
 
     return {
